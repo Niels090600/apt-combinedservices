966740ddd94b34a432fe49fd340a47863d2254f6
Arne Hus <r0793284@student.thomasmore.be>
2022-01-06 10:11:45 +0100

    restconfig added

---
+++ edge-service/src/main/java/com/example/aptedgeservice/controller/RestConfig.java
@@ -0,0 +1,22 @@
+package com.example.aptedgeservice.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Configuration
+public class RestConfig {
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(MediaType.ALL));
+        restTemplate.getMessageConverters().add(converter);
+
+        return restTemplate;
+    }
+}
